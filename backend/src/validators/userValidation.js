a757eac87b363f22f2a6877681940acd4bc3d1d1
Julien RICHARD <julien.y.richard@gmail.com>
2023-11-28 10:49:41 +0100

    ajout des routes users

---
+++ backend/src/validators/userValidation.js
@@ -0,0 +1,29 @@
+const Joi = require("joi");
+
+const getSchema = (req) => {
+  const option = req.method === "POST" ? "required" : "optional";
+  return Joi.object({
+    email: Joi.string().max(255).presence(option),
+    password: Joi.string().presence(option),
+    role_id: Joi.number().presence(option),
+  });
+};
+
+const userValidation = (req, res, next) => {
+  const schema = getSchema(req);
+
+  const { error } = schema.validate(
+    {
+      ...req.body,
+    },
+    { abortEarly: false }
+  );
+
+  if (error) {
+    res.status(422).json({ validationErrors: error.details });
+  } else {
+    next();
+  }
+};
+
+module.exports = userValidation;
