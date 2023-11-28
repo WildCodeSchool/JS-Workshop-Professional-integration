d923b8d86d7654d1811ac2b851df4f7540280f25
Julien RICHARD <julien.y.richard@gmail.com>
2023-11-28 15:31:28 +0100

    Ajout de la table pour les publisher

--- frontend/src/pages/admin/AdminPublisher.jsx
+++ frontend/src/pages/admin/AdminPublisher.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import connexion from "../../services/connexion";
+
+import tableModel from "../../tableSchema/publisher.json";
+import AdminTable from "../../components/AdminTable";
 
 function AdminPublisher() {
-  return <div>AdminPublisher</div>;
+  const [publishers, setPublishers] = useState([]);
+
+  const getPublishers = async () => {
+    try {
+      const myPublishers = await connexion.get("/publishers");
+      setPublishers(myPublishers.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const removePublisher = async (id) => {
+    try {
+      await connexion.delete(`/publishers/${id}`);
+      setPublishers((prev) => prev.filter((el) => el.id !== id));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPublishers();
+  }, []);
+
+  return (
+    <main>
+      <h2>Liste des publishers en cours</h2>
+      <AdminTable
+        data={publishers}
+        model={tableModel}
+        removeElement={removePublisher}
+      />
+    </main>
+  );
 }
 
 export default AdminPublisher;
