8fa304372e1da771e63eefd18fd0cad76767ef4a
Julien RICHARD <julien.y.richard@gmail.com>
2023-11-28 10:29:20 +0100

    Mise en place de la DB de base avec data

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,135 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+create table image (
+  id INT  primary key auto_increment not null,
+  src VARCHAR(255) NOT NULL,
+  alt text not null,
+  main boolean DEFAULT false,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  size int not null
 );
+
+create table author (
+  id int  primary key auto_increment not null,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  job_title VARCHAR(255) NOT NULL,
+  website VARCHAR(255),
+  facebook VARCHAR(255),
+  linkedIn VARCHAR(255),
+  avatar_id int,
+  description LONGTEXT NOT NULL,
+  birthday date,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,CONSTRAINT image_by_author_id_fk FOREIGN KEY (avatar_id) REFERENCES image(id)
+);
+
+create table publisher (
+  id INT primary key auto_increment not null,
+  name VARCHAR(255) NOT NULL,
+  website VARCHAR(255),
+  description LONGTEXT NOT NULL,
+  logo_id int,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT logo_id_fk FOREIGN KEY (logo_id) REFERENCES image(id)
+);
+
+create table article (
+  id int  primary key auto_increment not null,
+  title VARCHAR(255) NOT NULL,
+  subtitle VARCHAR(255) NOT NULL,
+  content LONGTEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  author_id INT NOT NULL,
+  publisher_id INT NOT NULL,
+  CONSTRAINT author_id_fk FOREIGN KEY (author_id) REFERENCES author(id),
+  CONSTRAINT publisher_id_fk FOREIGN KEY (publisher_id) REFERENCES publisher(id)
+);
+
+
+
+create table image_by_article (
+  id INT  primary key auto_increment not null,
+  image_id int not null,
+  article_id int not null,
+  CONSTRAINT image_by_article_id_fk FOREIGN KEY (image_id) REFERENCES image(id),
+  CONSTRAINT article_with_image_id_fk FOREIGN KEY (article_id) REFERENCES article(id)
+);
+
+create table category (
+  id INT  primary key auto_increment not null,
+  label VARCHAR(255) NOT NULL,
+  description LONGTEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table category_by_article (
+  id INT  primary key auto_increment not null,
+  article_id int not null,
+  category_id int not null,
+  CONSTRAINT category_to_article_id_fk FOREIGN KEY (article_id) REFERENCES article(id),
+  CONSTRAINT article_to_category_id_fk FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+
+
+create table role (
+  id INT  primary key auto_increment not null,
+  label VARCHAR(255) NOT NULL
+);
+
+create table user (
+  id INT  primary key auto_increment not null,
+  email VARCHAR(255) NOT NULL,
+  password text,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  role_id int not null,
+  CONSTRAINT role_id_fk FOREIGN KEY (role_id) REFERENCES role(id)
+);
+
+create table profil (
+  id INT  primary key auto_increment not null,
+  firstname VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  avatar VARCHAR(255) NOT NULL,
+  birthday date,
+  pseudo int,
+  user_id int not null,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT user_id_fk FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+
+
+create table vote_type (
+  id INT  primary key auto_increment not null,
+  label VARCHAR(255) NOT NULL
+);
+
+
+create table vote (
+  id int  primary key auto_increment not null,
+  article_id int not null,
+  user_id int not null,
+  type_id int not null,
+  CONSTRAINT article_to_vote_id_fk FOREIGN KEY (article_id) REFERENCES article(id),
+  CONSTRAINT user_to_vote_id_fk FOREIGN KEY (user_id) REFERENCES user(id),
+  CONSTRAINT type_id_fk FOREIGN KEY (type_id) REFERENCES vote_type(id)
+);
+
+
+create table comment (
+  id INT  primary key auto_increment not null,
+  text text not null,
+  user_id int not null,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT user_comment_id_fk FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+create table comment_by_article (
+  id INT  primary key auto_increment not null,
+  article_id int not null,
+  comment_id int not null,
+  CONSTRAINT comment_to_article_id_fk FOREIGN KEY (article_id) REFERENCES article(id),
+  CONSTRAINT article_to_comment_id_fk FOREIGN KEY (comment_id) REFERENCES comment(id)
+);
+
+
--- backend/seed.js
+++ backend/seed.js
@@ -20,14 +20,144 @@ const seed = async () => {
     // Generating Seed Data
 
     // Optional: Truncate tables (remove existing data)
-    await database.query("truncate item");
 
-    // Insert fake data into the 'item' table
-    for (let i = 0; i < 10; i += 1) {
+    // Insert fake data into the 'image' table for author relationship
+    for (let i = 0; i < 50; i += 1) {
       queries.push(
-        database.query("insert into item(title) values (?)", [
-          faker.lorem.word(),
-        ])
+        database.query(
+          "insert into image(src, alt, main, size) values (?, ?, ?, ?)",
+          [
+            faker.image.avatar(),
+            faker.lorem.words(),
+            false,
+            faker.number.float({ min: 1, max: 10, precision: 0.001 }),
+          ]
+        )
+      );
+    }
+    console.info("Image for author filled Correctly");
+
+    // Insert fake data into the 'author' table
+    for (let i = 0; i < 50; i += 1) {
+      queries.push(
+        database.query(
+          "insert into author(firstname, lastname, job_title, website, description, avatar_id, birthday) values (?, ?, ?, ?, ?, ?, ?)",
+          [
+            faker.person.firstName(),
+            faker.person.lastName(),
+            faker.person.jobTitle(),
+            faker.internet.url(),
+            faker.lorem.paragraphs(),
+            i + 1,
+            faker.date
+              .birthdate({ min: 1900, max: 2000, mode: "year" })
+              .toISOString()
+              .split("T")
+              .shift(),
+          ]
+        )
+      );
+    }
+    console.info("author filled Correctly");
+
+    // Insert fake data into the 'image' table for publisher relationship
+    for (let i = 0; i < 20; i += 1) {
+      queries.push(
+        database.query(
+          "insert into image(src, alt, main, size) values (?, ?, ?, ?)",
+          [
+            faker.image.urlLoremFlickr({
+              category: "technics",
+              height: 80,
+              width: 80,
+            }),
+            faker.lorem.words(),
+            false,
+            faker.number.float({ min: 1, max: 10, precision: 0.001 }),
+          ]
+        )
+      );
+    }
+    console.info("Image for publisher filled Correctly");
+
+    // Insert fake data into the 'publisher' table
+    for (let i = 0; i < 20; i += 1) {
+      queries.push(
+        database.query(
+          "insert into publisher(name, website, description, logo_id) values (?, ?, ?, ?)",
+          [
+            faker.company.name(),
+            faker.internet.url(),
+            faker.lorem.paragraphs(),
+            i + 51,
+          ]
+        )
+      );
+    }
+    console.info("Publisher filled Correctly");
+
+    // Insert fake data into the 'article' table
+    for (let i = 0; i < 250; i += 1) {
+      queries.push(
+        database.query(
+          "insert into article(title, subtitle, content, author_id, publisher_id) values (?, ?, ?, ?, ?)",
+          [
+            faker.lorem.words({ min: 4, max: 8 }),
+            faker.lorem.lines({ min: 1, max: 3 }),
+            faker.lorem.paragraphs({ min: 3, max: 5 }),
+            Math.ceil(Math.random() * 50),
+            Math.ceil(Math.random() * 20),
+          ]
+        )
+      );
+    }
+    console.info("Article filled Correctly");
+
+    // Insert Image in the table for the many to many article relationship
+    for (let i = 0; i < 1250; i += 1) {
+      queries.push(
+        database.query(
+          "insert into image(src, alt, main, size) values (?, ?, ?, ?)",
+          [
+            faker.image.urlLoremFlickr({
+              category: "technics",
+            }),
+            faker.lorem.words(),
+            i % 5 === 0,
+            faker.number.float({ min: 1, max: 10, precision: 0.001 }),
+          ]
+        )
+      );
+    }
+
+    // Mise en place de la relation images / articles
+    for (let i = 0; i < 1250; i += 1) {
+      queries.push(
+        database.query(
+          "insert into image_by_article(image_id, article_id) values (?, ?)",
+          [i + 1, Math.floor(i / 5) + 1]
+        )
+      );
+    }
+
+    // Insert of the category
+    for (let i = 0; i < 30; i += 1) {
+      queries.push(
+        database.query(
+          "insert into category(label, description) values (?, ?)",
+          [faker.lorem.word(), faker.lorem.lines({ min: 1, max: 3 })]
+        )
+      );
+    }
+
+    for (let i = 0; i < 250; i += 1) {
+      const article = i + 1;
+      const rand = Math.ceil(Math.random() * 10);
+      queries.push(
+        database.query(
+          "insert into category_by_article(article_id, category_id) values (?, ?), (?, ?), (?, ?)",
+          [article, rand, article, rand + 10, article, rand + 20]
+        )
       );
     }
 
